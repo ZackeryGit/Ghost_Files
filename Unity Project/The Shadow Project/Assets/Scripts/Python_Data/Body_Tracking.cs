34a0f9a98c6d3549c80505122a4dd49630470469
Braxton Brown <10944268@uvu.edu>
2025-09-30 08:58:35 -0600

    Revert "Body Tracking Testing scene and UDP setup"
    This reverts commit 7e3b7cbb8eee51f459648ea8b75646c846049948.

--- Unity Project/The Shadow Project/Assets/Scripts/Python_Data/Body_Tracking.cs
+++ Unity Project/The Shadow Project/Assets/Scripts/Python_Data/Body_Tracking.cs
@@ -2,30 +2,15 @@ using UnityEngine;
 
 public class Body_Tracking : MonoBehaviour
 {
-
-    public UDP udpBody;
-    public GameObject bodyPoint; // Single GameObject to move
-
-    private void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        string data = udpBody.data;
         
-        data = data.Trim(new char[] { '[', ']' });
-        string[] points = data.Split(',');
+    }
 
-        // Ensure there are enough points
-        if (points.Length < 2)
-        {
-            Debug.LogError("Not enough points received.");
-            return;
-        }
-
-        // Parse the first landmark position
-        // x-xis is minused by 7 because it offenses the position of the body by 7 when transferred
-        float x = 7 - float.Parse(points[0]) / 100;
-        float y = float.Parse(points[1]) / 100;
+    // Update is called once per frame
+    void Update()
+    {
         
-        // Update the position of the single GameObject
-        bodyPoint.transform.localPosition = new Vector3(x, y, bodyPoint.transform.localPosition.z);
     }
 }
